55c3677eb5bae861259a52af3082e861b7748a2a
LeonardoFrazzetto <leonardo.frazzetto1990@gmail.com>
2021-06-11 13:13:59 +0200

    Agregado Trozo 4

--- api.py
+++ api.py
@@ -7,7 +7,14 @@ def landing_page():
     return render_template('index.html')
 
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template('ÑAÑAAÑÑAÑAÑAÑAÑÑA.html')
 
+
+@app.route("/map")
+def map():
+    return render_template('map.html')
 
 
 
